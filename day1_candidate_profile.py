149eb355c1470357be0aba0595b35b09358ced5c
Akshita <diwanakshita2005@gmail.com>
2026-03-18 18:41:32 +0530

    Add candidate profile information in day1_candidate_profile.py

---
+++ day1_candidate_profile.py
@@ -0,0 +1,17 @@
+candidate_name = "Akshita"
+candidate_age = 20
+candidate_branch = "BTech CSE"
+candidate_college = "IITM College of Engineering"
+candidate_skills = "Python, Communication"
+candidate_semester_cgpa = 8.5
+candidate_goal = "Top 0.5% Founder"
+candidate_projects = "None yet"
+
+print(candidate_name)
+print(candidate_age)
+print(candidate_branch)
+print(candidate_college)
+print(candidate_skills)
+print(candidate_semester_cgpa)
+print(candidate_goal)
+print(candidate_projects)
